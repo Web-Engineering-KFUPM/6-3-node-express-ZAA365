4a21964566fa8a6f8787711b11696d0d51069555
ALAHMED	ZAHRAA <s202344670@ccm.kfupm.edu.sa>
2026-04-06 13:27:12 +0300

    TODO 3:Create getRandomQuote Function

--- 6-3-node-express/backend/quotes.js
+++ 6-3-node-express/backend/quotes.js
@@ -1,5 +1,5 @@
 // Import random.js file
-
+import { getRandomInt } from "./utils/random.js";
 const quotes = [
   "Code is like humor. When you have to explain it, it’s bad.",
   "Before software can be reusable it first has to be usable.",
@@ -10,3 +10,25 @@ const quotes = [
 // TODO 3: Create and export getRandomQuote function
 //Hint: export function getRandomQuote(){}
 
+export function getRandomQuote() {
+  const randomIndex = getRandomInt(quotes.length);
+  return quotes[randomIndex];}
+  
+/*TODO 3:Create getRandomQuote Function
+
+In quotes.js, export a function named getRandomQuote.
+
+Goal:
+Return one random quote from the quotes array.
+
+Important:
+- Use getRandomInt(max)
+- Import random.js file to quote.js to use getRandomInt function.
+
+Steps:
+- Generate random index
+- Return quote using that index
+
+Syntax hint:
+   const index = ______(quotes.length);
+   return ______[index];*/
